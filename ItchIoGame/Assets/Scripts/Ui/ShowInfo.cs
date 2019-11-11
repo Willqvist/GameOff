e9ded9937b713f6f2e3b75c13a46d5d0022508a3
Willqvist <willqvist@gmail.com>
2019-11-11 18:05:41 +0100

    UI update
    UI update

---
+++ ItchIoGame/Assets/Scripts/Ui/ShowInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ShowInfo : IClickHandler
+{
+    public Animator parentAnimator;
+    public TextMeshProUGUI text;
+    private bool active = false;
+
+    public override void OnClick(ButtonController button)
+    {
+        active = !active;
+        text.SetText(active ? "X Info" : "? Info");
+        parentAnimator.SetBool("isActive", active);
+    }
+}

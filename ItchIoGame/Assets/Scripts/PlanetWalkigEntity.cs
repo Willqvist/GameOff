9f0b9ba2a42527608de5291fe67b9f95fd6d329f
Mojkoo <33422251+Mojkoo@users.noreply.github.com>
2019-11-11 18:54:08 +0100

    Small ref + EntityData

--- ItchIoGame/Assets/Scripts/PlanetWalkigEntity.cs
+++ ItchIoGame/Assets/Scripts/PlanetWalkigEntity.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 
 public class PlanetWalkigEntity : PlanetEntity
 {
-    // Start is called before the first frame update
     public Rigidbody rigidBody;
     public float moveSpeed = 5;
-    public virtual void Start()
+    public override void Start()
     {
         base.Start();
     }
 
-    // Update is called once per frame
-    public void Update()
+    public override void Update()
     {
         base.Update();
+
         if (hasPlanet()) {
             rigidBody.isKinematic = false;
             
@@ -25,13 +24,7 @@ public class PlanetWalkigEntity : PlanetEntity
             attract(this.transform,rigidBody);
         }
     }
-    /*
-    public override void SetColor(Color color)
-    {
-        //base.setColor();
 
-    }
-    */
     private void attract(Transform ptransform, Rigidbody body) {
         Vector3 up = (ptransform.position - planet.transform.position).normalized;
         Vector3 localUp = ptransform.up;
@@ -40,6 +33,7 @@ public class PlanetWalkigEntity : PlanetEntity
         Quaternion rot = Quaternion.FromToRotation(localUp,up) * ptransform.rotation;
         transform.rotation = Quaternion.Slerp(ptransform.rotation, rot, 50 * Time.deltaTime);
     }
+
     public override void OnPlace()
     {
         base.OnPlace();
---
+++ ItchIoGame/Assets/Scripts/Shop/ShopItemData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New ShopItem", menuName = "ShopItem")]
+public class ShopItemData : ScriptableObject
+{
+    public EntityData entityData;
+}
